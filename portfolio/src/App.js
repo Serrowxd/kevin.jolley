932e2edaf8d20ad221bb4a4a48f6f3ccc53c51cd
Kevin <serrowpls@gmail.com>
2024-05-14 09:01:19 -0500

    working on theme swap

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,10 +2,14 @@ import "./styles/App.css";
 import { useState } from "react";
 
 function App() {
-  const [theme, setTheme] = useState(false);
+  const [theme, setTheme] = useState(true);
+
+  const themeHandler = () => {
+    setTheme(!theme);
+  };
 
   return (
-    <div className={"container-dark"}>
+    <div className={theme ? "container-dark" : "container-light"}>
       <div className={"header"}>
         <h1>Kevin Jolley</h1>
         <p>Full-Stack Software Engineer</p>
@@ -15,6 +19,7 @@ function App() {
           <p>About</p>
           <p>Projects</p>
           <p>Hobbies</p>
+          <p onClick={themeHandler}>{theme ? "Light" : "Dark"}</p>
         </div>
       </div>
     </div>
