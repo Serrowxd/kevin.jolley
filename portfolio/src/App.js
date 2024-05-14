c5417edacc7842c46e92a6ccaf044e96b3d0c20e
Kevin <serrowpls@gmail.com>
2024-05-14 00:18:53 -0500

    Homepage, a little CSS, and some additional boilerplate setup

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/App.css";
+import { useState } from "react";
 
 function App() {
+  const [theme, setTheme] = useState(false);
+
+  const themeHandler = () => {
+    setTheme(!theme);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={theme ? "container" : "container-dark"}>
+      <div className={"header"}>
+        <h1>Kevin Jolley</h1>
+        <p>Full-Stack Software Engineer</p>
+      </div>
+      <div className={"header-nav"}>
+        <div className={"nav-container"}>
+          <p>About</p>
+          <p>Projects</p>
+          <p>Hobbies</p>
+          <p onClick={themeHandler}> {theme ? "Dark" : "Light"}</p>
+        </div>
+      </div>
     </div>
   );
 }
