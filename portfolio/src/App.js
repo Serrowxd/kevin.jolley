a36925dcffadbb7d25a57774b12d03ecdb800ecf
Kevin <serrowpls@gmail.com>
2024-05-16 18:17:42 -0500

    added TSParticles, not working so need to adjust, misc styling

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,5 +1,7 @@
-import "./styles/App.css";
+import "./styles/brutalism.css";
 import { useState } from "react";
+import logo from "./assets/Serrow_C.png";
+import { Particles } from "@tsparticles/react";
 
 function App() {
   const [theme, setTheme] = useState(true);
@@ -9,19 +11,45 @@ function App() {
   };
 
   return (
-    <div className={theme ? "container-dark" : "container-light"}>
-      <div className={"header"}>
-        <h1>Kevin Jolley</h1>
-        <p>Full-Stack Software Engineer</p>
-      </div>
-      <div className={"header-nav"}>
-        <div className={"nav-container"}>
-          <p>About</p>
-          <p>Projects</p>
-          <p>Hobbies</p>
-          <p onClick={themeHandler}>{theme ? "Light" : "Dark"}</p>
+    <div className={"main"}>
+      <Particles
+        id="particles"
+        options={{
+          background: {
+            color: "#000000",
+          },
+          particles: {
+            color: {
+              value: "#ffffff",
+            },
+            number: {
+              value: 200,
+            },
+            move: {
+              enable: true,
+              speed: 5,
+            },
+          },
+        }}
+      />
+
+      <div className={"main_background_splitter"}></div>
+      <nav className={"nav_container"}>
+        <div className={"nav_logo"}>
+          <img src={logo} alt="Logo" />
         </div>
-      </div>
+      </nav>
+
+      <section className={"section title"}>
+        <div className={"title_container"}>
+          <div className={"title_splitter"}>
+            <h1 className={"main_title_left"}>KVN</h1>
+            <h1 className={"main_splitter"}>.</h1>
+            <h1 className={"main_title_right"}>DEV</h1>
+          </div>
+          <h2 className={"title_label"}></h2>
+        </div>
+      </section>
     </div>
   );
 }
